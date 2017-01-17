9f596a1b6a4c3b6a50393de161c91c82e7771f7e
Jim Fulton <jim@jimfulton.info>
2017-01-17 11:00:39 -0500

    Conversion from pickle to JSON
    This is a follow on from versions in
    https://github.com/jimfulton/zodb-jsonb-experiment, which was derived
    from https://github.com/jimfulton/xpickle which was derived from https://github.com/zopefoundation/zope.xmlpickle.
    
    This is a pretty major rewrite:
    
    - Based on pickletools for looser coupling to pickle implementations.
    
    - Exhaustive tests.
    
    - Even greater emphasis on usability of JSON.

---
+++ src/newt/db/jsonpickle.py
@@ -0,0 +1,369 @@
+"""Convert pickles to JSON
+
+The goal of the conversion is to produce JSON that is useful for
+indexing, querying and reporting in external systems like Postgres and
+Elastic Search.
+
+Usage:
+
+>>> apickle = pickle.dumps([1,2])
+>>> unpickler = JsonUnpickler(apickle)
+>>> json_string = unpickler.load()
+>>> unpickler.pos == len(apickle)
+True
+"""
+import binascii
+from six.moves import copyreg
+import _codecs
+import datetime
+import json
+import pickletools
+from struct import unpack
+
+class Bytes(object):
+
+    def __init__(self, data):
+        self.data = data
+
+    def json_reduce(self):
+        return {'::': 'hex', 'hex': binascii.b2a_hex(self.data)}
+
+def u64(v):
+    """Unpack an 8-byte string into a 64-bit long integer."""
+    try:
+        return unpack(">Q", v)[0]
+    except Exception:
+        if isinstance(v, Bytes):
+            v = v.data
+        elif not isinstance(v, bytes):
+            v = v.encode('latin1')
+        return unpack(">Q", v)[0]
+
+bytes_types = (Bytes, bytes, str)
+class Persistent(object):
+
+    def __init__(self, id):
+        if isinstance(id, bytes_types):
+            id = u64(id)
+        else:
+            assert (len(id) == 2 and
+                    isinstance(id[0], bytes_types) and
+                    isinstance(id[1], Global))
+            id = u64(id[0]), id[1].name
+        self.id = id
+
+    def json_reduce(self):
+        return {'::': 'persistent', 'id': self.id}
+
+class Global(object):
+
+    def __init__(self, module, name):
+        self.name = module + '.' + name
+
+    def json_reduce(self):
+        return {'::': 'global', 'name': self.name}
+
+class Instance(object):
+
+    id = state = None
+
+    def __init__(self, class_name, args):
+        self.class_name = class_name
+        self.args = args
+
+    def __setstate__(self, state):
+        self.state = state
+
+    def json_reduce(self):
+        state = self.state
+        if not isinstance(state, dict):
+            state = dict(state=state) if state else {}
+
+        state['::'] = self.class_name
+        if self.args:
+            state['::()'] = self.args
+        if self.id is not None:
+            state['::id'] = self.id
+
+        return state
+
+class Get(object):
+
+    def __init__(self, unpickler, id, v):
+        self.unpickler = unpickler
+        self.id = id
+        self.v = v
+
+    def json_reduce(self):
+        if self.unpickler.cyclic:
+            return {'::': 'ref', 'id': self.id}
+        else:
+            return self.v
+
+class Put(Get):
+
+    got = False
+
+    def __setstate__(self, state):
+        self.v.__setstate__(state)
+
+    def __setitem__(self, k, v):
+        self.v[k] = v
+
+    def append(self, i):
+        self.v.append(i)
+
+    def update(self, v):
+        self.v.update(v)
+
+    def extend(self, v):
+        self.v.extend(v)
+
+    def __nonzero__(self):
+        return bool(self.v)
+
+    def json_reduce(self):
+        v = self.v
+        if self.got and self.unpickler.cyclic:
+            if isinstance(v, Instance):
+                v.id = self.id
+            else:
+                v = {'::': 'shared', 'id': self.id, 'value': v}
+        return v
+
+def dt_bytes(v):
+    if isinstance(v, Bytes):
+        v = v.data
+    return v
+
+def reconstruct(args):
+    (cls, base, state) = args
+    ob = Instance(cls.name, ())
+    if state is not None:
+        ob.__setstate__(state)
+    return ob
+
+def handle_set(args):
+    [arg] = args
+    if isinstance(arg, Put):
+        arg = arg.v
+    return sorted(arg)
+
+special_classes = {
+    'datetime.datetime':
+    lambda args: datetime.datetime(dt_bytes(*args)).isoformat(),
+    'datetime.date': lambda args: datetime.date(dt_bytes(*args)).isoformat(),
+    '_codecs.encode': lambda args: _codecs.encode(*args),
+    'copy_reg._reconstructor': reconstruct,
+    '__builtin__.frozenset': handle_set,
+    '__builtin__.set': handle_set,
+    'builtins.frozenset': handle_set,
+    'builtins.set': handle_set,
+    }
+
+def instance(global_, args):
+    name = global_.name
+    if name in special_classes:
+        return special_classes[name](args)
+
+    return Instance(name, args)
+
+basic_types = (float, int, type(1<<99), type(b''), type(u''),
+               tuple, Global, Bytes, frozenset)
+
+def default(ob):
+    try:
+        return ob.json_reduce()
+    except AttributeError:
+        if isinstance(ob, bytes):
+            try:
+                return ob.decode('ascii')
+            except Exception:
+                return {'::': 'hex', 'hex': binascii.b2a_hex(ob)}
+
+class JsonUnpickler:
+
+    cyclic = False
+
+    def __init__(self, pickle):
+        self.stack = []
+        self.append = self.stack.append
+        self.marks = []
+        self.memo = {}
+        self.set_pickle(pickle)
+
+    def set_pickle(self, pickle):
+        self.pickle = pickle
+        self.ops = pickletools.genops(pickle)
+
+    def load(self, **json_args):
+        for op, arg, pos in self.ops:
+            # print(op.name, arg, pos, self.stack) # uncomment to trace
+            if op.name == 'STOP':
+                self.pos = pos + 1
+                self.set_pickle(self.pickle[self.pos:])
+                try:
+                    return json.dumps(self.stack[-1],
+                                      default=default,
+                                      **json_args)
+                except ValueError:
+                    self.cyclic = True
+                    return json.dumps(self.stack[-1],
+                                      default=default,
+                                      **json_args)
+
+            getattr(self, op.name)(arg)
+
+    def push_arg(self, arg):
+        self.append(arg)
+
+    INT = BININT = BININT1 = BININT2 = push_arg
+    LONG = LONG1 = LONG4 = push_arg
+
+    def STRING(self, v):
+        if isinstance(v, bytes):
+            return self.BINBYTES(v)
+        self.append(v)
+    BINSTRING = SHORT_BINSTRING = STRING
+
+    def BINBYTES(self, v):
+        try:
+            v.decode('ascii')
+        except UnicodeDecodeError:
+            v = Bytes(v)
+        self.append(v)
+    SHORT_BINBYTES = BINBYTES8 = BINBYTES
+
+    UNICODE = BINUNICODE = SHORT_BINUNICODE = BINUNICODE8 = push_arg
+    FLOAT = BINFLOAT = push_arg
+    STOP = None # for checking completeness
+
+    def        NONE(self, _): self.push_arg( None )
+    def     NEWTRUE(self, _): self.push_arg( True )
+    def    NEWFALSE(self, _): self.push_arg( False )
+    def  EMPTY_LIST(self, _): self.push_arg( [] )
+    def EMPTY_TUPLE(self, _): self.push_arg( () )
+    def  EMPTY_DICT(self, _): self.push_arg( {} )
+    def   EMPTY_SET(self, _): self.push_arg( [] )
+
+    def APPEND(self, _):
+        ob = self.stack.pop()
+        self.stack[-1].append(ob)
+
+    def pop(self, count):
+        result = self.stack[-count:]
+        self.stack[-count:] = []
+        return result
+
+    def pop_marked(self):
+        mark = self.marks.pop()
+        marked = self.stack[mark:]
+        self.stack[mark:] = []
+        return marked
+
+    def APPENDS(self, _):
+        marked = self.pop_marked()
+        self.stack[-1].extend(marked)
+
+    def LIST(self, _):
+        marked = self.pop_marked()
+        self.append(marked)
+
+    def TUPLE(self, _):
+        marked = self.pop_marked()
+        self.append(tuple(marked))
+
+    def TUPLE1(self, _): self.stack[-1] = (self.stack[-1], )
+    def TUPLE2(self, _): self.stack[-2:] = [tuple(self.stack[-2:])]
+    def TUPLE3(self, _): self.stack[-3:] = [tuple(self.stack[-3:])]
+
+    def DICT(self, _):
+        marked = self.pop_marked()
+        self.append({marked[i]: marked[i+1]
+                             for i in range(0, len(marked), 2)
+                             })
+
+    def SETITEM(self, _):
+        k, v = self.pop(2)
+        self.stack[-1][k] = v
+
+    def SETITEMS(self, _):
+        marked = self.pop_marked()
+        self.stack[-1].update({marked[i]: marked[i+1]
+                                 for i in range(0, len(marked), 2)
+                                 })
+
+    def ADDITEMS(self, _):
+        marked = self.pop_marked()
+        self.stack[-1].extend(sorted(marked))
+
+    def FROZENSET(self, _):
+        marked = self.pop_marked()
+        self.append(sorted(marked))
+
+    def POP(self, _): self.stack.pop()
+    def DUP(self, _): self.append(self.stack[-1])
+    def MARK(self, _): self.marks.append(len(self.stack))
+    def POP_MARK(self, _): self.pop_marked()
+
+    def GET(self, k):
+        v = self.memo[k]
+        if not isinstance(v, basic_types):
+            v.got = True
+            v = Get(self, k, v.v)
+        self.append(v)
+    BINGET = LONG_BINGET = GET
+
+    def PUT(self, k):
+        v = self.stack.pop()
+        if not isinstance(v, basic_types):
+            v = Put(self, k, v)
+        self.append(v)
+        self.memo[k] = v
+    BINPUT = LONG_BINPUT = PUT
+
+    def MEMOIZE(self, _):
+        self.PUT(len(self.memo))
+
+    def EXT1(self, code):
+        self.append(Global(*copyreg._inverted_registry[code]))
+    EXT2 = EXT4 = EXT1
+
+    def GLOBAL(self, arg):
+        self.append(Global(*arg.split()))
+
+    def STACK_GLOBAL(self, _):
+        args = self.pop(2)
+        self.append(Global(*args))
+
+    def REDUCE(self, _):
+        f, args = self.pop(2)
+        self.append(instance(f,args))
+
+    def BUILD(self, _):
+        state = self.stack.pop()
+        self.stack[-1].__setstate__(state)
+
+    def INST(self, arg):
+        self.append(instance(Global(*arg.split()), tuple(self.pop_marked())))
+
+    def OBJ(self, _):
+        args = self.pop_marked()
+        self.append(instance(args[0], tuple(args[1:])))
+
+    def NEWOBJ(self, _):
+        self.append(instance(*self.pop(2)))
+
+    def NEWOBJ_EX(self, _):
+        cls, args, kw = self.pop(3)
+        self.append(instance(cls, (args, kw)))
+
+    def PROTO(self, _): pass
+    def FRAME(self, _): pass
+
+    def PERSID(self, id):           # pragma: no cover
+        self.append(Persistent(id)) # Not used because only proto 0
+                                    # and ZODB uses 1 or 3 (or greater later).
+
+    def BINPERSID(self, _):
+        self.stack[-1] = Persistent(self.stack[-1])
---
+++ src/newt/db/tests/testjsonpickle.py
@@ -0,0 +1,365 @@
+##############################################################################
+#
+# Copyright (c) Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from six.moves import copyreg
+import datetime
+import json
+from persistent.mapping import PersistentMapping
+import pickle
+from pprint import pprint
+from six import BytesIO, PY3
+import textwrap
+import unittest
+import ZODB
+from ZODB.utils import z64, p64, maxtid
+
+from ..jsonpickle import JsonUnpickler
+
+class C(object):
+    pass
+
+class I:
+    pass
+
+class E190(object): pass
+class E60190(object): pass
+class E600000190(object): pass
+copyreg.add_extension(__name__, 'E190', 190)
+copyreg.add_extension(__name__, 'E60190', 60190)
+copyreg.add_extension(__name__, 'E600000190', 600000190)
+
+test_many_pickle_expect = """\
+{
+  "l1": [
+    "spam ",
+    "spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam ",
+    {
+      "::": "hex",
+      "hex": "dd"
+    },
+    1.23,
+    "2017-01-02",
+    {
+      "f": false,
+      "l": [],
+      "l1": [
+        1
+      ],
+      "n": null,
+      "t": true
+    },
+    "2017-01-02T04:05:06",
+    9,
+    99,
+    1073741824,
+    1152921504606846976,
+    1237940039285380274899124224,
+    [
+      "spam ",
+      "spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam ",
+      {
+        "::": "hex",
+        "hex": "dd"
+      },
+      1.23,
+      "2017-01-02",
+      {
+        "f": false,
+        "l": [],
+        "l1": [
+          1
+        ],
+        "n": null,
+        "t": true
+      },
+      "2017-01-02T04:05:06",
+      9,
+      99,
+      1073741824,
+      1152921504606846976,
+      1237940039285380274899124224
+    ]
+  ],
+  "l2": [
+    "spam ",
+    "spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam ",
+    {
+      "::": "hex",
+      "hex": "dd"
+    },
+    1.23,
+    "2017-01-02",
+    {
+      "f": false,
+      "l": [],
+      "l1": [
+        1
+      ],
+      "n": null,
+      "t": true
+    },
+    "2017-01-02T04:05:06",
+    9,
+    99,
+    1073741824,
+    1152921504606846976,
+    1237940039285380274899124224,
+    [
+      "spam ",
+      "spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam spam ",
+      {
+        "::": "hex",
+        "hex": "dd"
+      },
+      1.23,
+      "2017-01-02",
+      {
+        "f": false,
+        "l": [],
+        "l1": [
+          1
+        ],
+        "n": null,
+        "t": true
+      },
+      "2017-01-02T04:05:06",
+      9,
+      99,
+      1073741824,
+      1152921504606846976,
+      1237940039285380274899124224
+    ]
+  ]
+}"""
+
+special = object()
+class SpecialPickler(pickle.Pickler):
+
+    def persistent_id(self, obj):
+        if obj is special:
+            return b'\xff' * 8
+
+
+class JsonUnpicklerProtocol0Tests(unittest.TestCase):
+
+    maxDiff = None
+
+    proto = 0
+
+    def test_many_pickle(self):
+        s = 'spam '
+        S = s*99
+        b = b'\xdd'
+        f = 1.23
+        d = datetime.date(2017, 1, 2)
+        di = dict(n=None, t=True, f=False, l=[], l1=[1])
+        t = datetime.datetime(2017, 1, 2, 4, 5, 6)
+        tup = s, S, b, f, d, di, t, 9, 99, 1 << 30, 1 << 60, 1 << 90
+        lst = list(tup)
+        lst.append(tup)
+        data = dict(l1 = lst, l2=lst)
+        got = (
+            JsonUnpickler(pickle.dumps(data, self.proto))
+            .load(sort_keys=True, indent=2)
+            .replace(' \n', '\n')
+            )
+        self.assertEqual(test_many_pickle_expect, got)
+
+    def test_unicode(self):
+        data = u'\ua000', u'\ua000\n\ua000'
+        got = JsonUnpickler(pickle.dumps(data, self.proto)).load(sort_keys=True)
+        self.assertEqual(got, '["\\ua000", "\\ua000\\n\\ua000"]')
+
+    def test_cyclic_list(self):
+        data = [1, 2]
+        data.append((3, data))
+        got = JsonUnpickler(pickle.dumps(data, self.proto)).load(sort_keys=True)
+        self.assertEqual(
+            got,
+            '{"::": "shared", "id": 0, "value":'
+            ' [1, 2, [3, {"::": "ref", "id": 0}]]}'
+            )
+
+    def test_cyclic_object(self, cls=C):
+        c = cls(); c.name = 'c'; c.c = c
+        got = json.loads(JsonUnpickler(pickle.dumps(c, self.proto)).load())
+        self.assertEqual(got.pop('::id'), got['state']['c'].pop('id'))
+        self.assertEqual(
+            got, {"::": "newt.db.tests.testjsonpickle." + cls.__name__,
+                  "state": {"c": {"::": "ref"},
+                            "name": "c"}})
+
+    def test_cyclic_instance(self):
+        self.test_cyclic_object(I)
+
+    def test_sets(self):
+        data = set((1,2,3)), frozenset((4, 5, 6))
+        got = JsonUnpickler(pickle.dumps(data, self.proto)).load()
+        self.assertEqual(got, '[[1, 2, 3], [4, 5, 6]]')
+
+class JsonUnpicklerProtocol1Tests(JsonUnpicklerProtocol0Tests):
+
+    def test_scalar_persistent_id(self):
+        data = (special, special)
+        f = BytesIO()
+        pickler = SpecialPickler(f, protocol=self.proto)
+        pickler.dump(data)
+        got = JsonUnpickler(f.getvalue()).load(sort_keys=True)
+        self.assertEqual(
+            got,
+            '[{"::": "persistent", "id": 18446744073709551615},'
+            ' {"::": "persistent", "id": 18446744073709551615}]')
+
+    proto = 1
+
+class JsonUnpicklerProtocol2Tests(JsonUnpicklerProtocol0Tests):
+
+    proto = 2
+
+    def test_ext(self):
+        data = E190(), E60190(), E600000190()
+        got = JsonUnpickler(pickle.dumps(data, self.proto)).load()
+        self.assertEqual(
+            got,
+            '[{"::": "newt.db.tests.testjsonpickle.E190"},'
+            ' {"::": "newt.db.tests.testjsonpickle.E60190"},'
+            ' {"::": "newt.db.tests.testjsonpickle.E600000190"}]')
+
+if pickle.HIGHEST_PROTOCOL >=3:
+
+    class JsonUnpicklerProtocol3Tests(JsonUnpicklerProtocol0Tests):
+
+        proto = 3
+
+    if pickle.HIGHEST_PROTOCOL >=4:
+
+        class JsonUnpicklerProtocol4Tests(JsonUnpicklerProtocol0Tests):
+
+            proto = 4
+
+            def test_newobj_ex(self):
+                class X:
+
+                    def __reduce_ex__(self, proto):
+                        assert proto >= 4
+                        return copyreg.__newobj_ex__, (C, (1,2), dict(c=3))
+
+                got = JsonUnpickler(pickle.dumps(X(), self.proto)
+                                    ).load(sort_keys=True)
+                self.assertEqual(
+                    got,
+                    '{"::": "newt.db.tests.testjsonpickle.C",'
+                    ' "::()": [[1, 2], {"c": 3}]}')
+
+class Complete(unittest.TestCase):
+
+    def test_handle_all_opcodes(self):
+        import pickletools
+        for opcode in pickletools.opcodes:
+            self.assertTrue(hasattr(JsonUnpickler, opcode.name))
+
+class JsonUnpicklerDBTests(unittest.TestCase):
+
+    maxDiff = None
+
+    def setUp(self):
+        self.db = ZODB.DB(None)
+        self.conn = self.db.open()
+        self.root = self.conn.root
+
+    def tearDown(self):
+        self.db.close()
+
+    def commit(self, p=None):
+        if p is None:
+            self.conn.transaction_manager.commit()
+            p, _, _ = self.db.storage.loadBefore(z64, maxtid)
+        self.unpickler = JsonUnpickler(p)
+        return p
+
+    def load(self):
+        return json.loads(self.unpickler.load())
+
+    def pprint(self):
+        pprint(self.load())
+
+    def check(self, expected):
+        self.assertEqual(self.load(), expected)
+
+    def test_basics(self):
+        root = self.root
+        root.numbers = 0, 123456789, 1 << 70, 1234.56789
+        root.time = datetime.datetime(2001, 2, 3, 4, 5, 6, 7)
+        root.date = datetime.datetime(2001, 2, 3)
+        root.delta = datetime.timedelta(1, 2, 3)
+        root.name = u'root'
+        root.data = b'\xff'
+        root.list = [1, 2, 3, root.name, root.numbers]
+        root.first = PersistentMapping()
+        p = self.commit()
+        self.check({"::": "global",
+                    "name": "persistent.mapping.PersistentMapping"})
+
+        # pos points just past end of pickle:
+        self.assertEqual(p[self.unpickler.pos-1:self.unpickler.pos], b'.')
+
+        self.check(
+            {u'data':
+              {u'data': {u'::': u'hex', u'hex': u'ff'},
+               u'date': u'2001-02-03T00:00:00',
+               u'delta': {u'::': u'datetime.timedelta',
+                          u'::()': [1, 2, 3]},
+               u'first': {u'::': u'persistent',
+                          u'id': [1, u'persistent.mapping.PersistentMapping']},
+               u'list': [1, 2, 3, u'root',
+                         [0, 123456789, 1180591620717411303424, 1234.56789]],
+               u'name': u'root',
+               u'numbers': [0, 123456789, 1180591620717411303424, 1234.56789],
+               u'time': u'2001-02-03T04:05:06.000007'}}
+            )
+
+    def test_non_ascii_zoid(self):
+        root = self.root
+        for i in range(200):
+            self.conn.add(PersistentMapping())
+        root.x = PersistentMapping()
+        self.commit()
+        _ = self.load()
+        _ = self.load()
+
+    def test_put_append(self):
+        root = self.root
+        self.root.x = self.root.y = [1]
+        self.commit()
+        _ = self.load()
+        _ = self.load()
+
+    def test_put_setitem(self):
+        root = self.root
+        self.root.x = self.root.y = dict(x=1)
+        self.commit()
+        _ = self.load()
+        _ = self.load()
+
+    def test_put_persistent_id(self):
+        self.commit(
+           b'cBTrees.OOBTree\nOOBTree\nq\x01.((((U\x07100x100q\x02(U\x08'
+           b'\x00\x00\x00\x00\x00\x92s\x11q\x03ccontent.models.files\n'
+           b'Thumbnail\nq\x04tq\x05QU\x0550x50q\x06(U\x08\x00\x00\x00'
+           b'\x00\x00\x9cV_q\x07h\x04tq\x08QU\x0675x100q\t(U\x08\x00'
+           b'\x00\x00\x00\x00\x92s\x0eq\nh\x04tq\x0bQU\x0585x85q\x0c'
+           b'(U\x08\x00\x00\x00\x00\x00\x9cV]q\rh\x04tq\x0eQttttq\x0f.')
+
+        _ = self.load()
+        _ = self.load()
+
