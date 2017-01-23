f943c45c2f3c23f364a66a1cba44b3d0268fb0ec
Jim Fulton <jim@zope.com>
2017-01-23 11:52:56 -0500

    Added ZConfig support for Newt database objects

--- src/newt/db/component.py
+++ src/newt/db/component.py
@@ -1,4 +1,5 @@
-from relstorage.options import Options
+import relstorage.storage
+import relstorage.adapters.postgresql
 
 class Adapter:
 
@@ -9,3 +10,22 @@ class Adapter:
         from ._adapter import Adapter
         return Adapter(dsn=self.config.dsn, options=options)
 
+class DB:
+
+    def __init__(self, config):
+        self.config = config
+        self.name = config.getSectionName()
+
+    def open(self, databases=None):
+        db = self.config.db
+        db.name = db.name or self.name
+        db = db.open(databases)
+        assert (
+            isinstance(db.storage,
+                       relstorage.storage.RelStorage)
+            and
+            isinstance(db.storage._adapter,
+                       relstorage.adapters.postgresql.PostgreSQLAdapter)
+            ), "Invalid storage"
+        from ._db import NewtDB
+        return NewtDB(db)
---
+++ src/newt/db/tests/testdbconfig.py
@@ -0,0 +1,72 @@
+import unittest
+from ZODB.config import databaseFromString
+
+from .base import DBSetup
+
+class DBConfigTest(DBSetup, unittest.TestCase):
+
+    def test_must_have_relstorage(self):
+        with self.assertRaisesRegexp(AssertionError, "Invalid storage"):
+            databaseFromString("""\
+                %import newt.db
+
+                <newtdb foo>
+                  <zodb>
+                  <mappingstorage>
+                  </mappingstorage>
+                  </zodb>
+                </newtdb>
+            """)
+
+    def test_just_relstorage(self):
+        db = databaseFromString("""\
+            %%import newt.db
+            %%import relstorage
+
+            <newtdb foo>
+              <zodb>
+                <relstorage>
+                  <postgresql>
+                    dsn dbname=%s
+                  </postgresql>
+                </relstorage>
+              </zodb>
+            </newtdb>
+            """ % self.dbname)
+
+        from .._db import NewtDB
+        self.assertEqual(db.__class__, NewtDB)
+        from relstorage.storage import RelStorage
+        self.assertEqual(db.storage.__class__, RelStorage)
+        from relstorage.adapters.postgresql import PostgreSQLAdapter
+        self.assertEqual(db.storage._adapter.__class__, PostgreSQLAdapter)
+
+        db.close()
+
+    def test_newt(self):
+        db = databaseFromString("""\
+            %%import newt.db
+            %%import relstorage
+
+            <newtdb foo>
+              <zodb>
+                <relstorage>
+                  <newt>
+                    <postgresql>
+                      dsn dbname=%s
+                    </postgresql>
+                  </newt>
+                </relstorage>
+              </zodb>
+            </newtdb>
+            """ % self.dbname)
+
+        from .._db import NewtDB
+        self.assertEqual(db.__class__, NewtDB)
+        from relstorage.storage import RelStorage
+        self.assertEqual(db.storage.__class__, RelStorage)
+        from .._adapter import Adapter
+        self.assertEqual(db.storage._adapter.__class__, Adapter)
+
+        db.close()
+
