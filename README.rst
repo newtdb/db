66a03a0bbd39dc701e8702c65f3ed76c011be003
Jim Fulton <jim@zope.com>
2017-01-24 10:55:49 -0500

    0.1.0

--- README.rst
+++ README.rst
@@ -2,14 +2,14 @@
 Newt DB, the amphibious database
 ================================
 
-Python objects above, Postgtresql jsonb below.
+Python objects above, Postgtresql JSONB below.
 
 See http://www.newtdb.org
 
 Changes
 =======
 
-0.1.0 (yyyy-mm-dd)
+0.1.0 (2017-01-24)
 ==================
 
 Initial release
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-name, version = 'newt.db', '0'
+name, version = 'newt.db', '0.1.0'
 
 install_requires = ['setuptools', 'RelStorage[postgresql]', 'psycopg2', 'six']
 extras_require = dict(test=['manuel', 'mock', 'zope.testing', 'zc.zlibstorage'])
@@ -10,10 +10,28 @@ from setuptools import setup
 
 long_description=open('README.rst').read()
 
+classifiers = """\
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Topic :: Database
+Topic :: Software Development :: Libraries :: Python Modules
+Operating System :: Microsoft :: Windows
+Operating System :: Unix
+Framework :: ZODB
+"""
+
 setup(
     author = 'Jim Fulton',
     author_email = 'jim@jimfulton.info',
     license = 'MIT',
+    url = 'http://www.newtdb.org/',
 
     name = name, version = version,
     long_description = long_description,
@@ -22,10 +40,10 @@ setup(
     namespace_packages = [name.split('.')[0]],
     package_dir = {'': 'src'},
     install_requires = install_requires,
+    keywords="database nosql python postgresql postgres",
     zip_safe = False,
     entry_points=entry_points,
     package_data = {name: ['*.txt', '*.test', '*.html']},
     extras_require = extras_require,
     tests_require = extras_require['test'],
-    test_suite = name+'.tests.test_suite',
     )
