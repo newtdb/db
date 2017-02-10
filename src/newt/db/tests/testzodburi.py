0017b7c5c621efaa9badf8c7a1573f17c0fdad15
Jim Fulton <jim@zope.com>
2017-02-10 12:10:49 -0500

    make parse a top-level function
    Making it usable in other tests (documentation)

--- src/newt/db/tests/testzodburi.py
+++ src/newt/db/tests/testzodburi.py
@@ -2,19 +2,19 @@ import mock
 import pkg_resources
 import unittest
 
+def parse(uri):
+    resolve_uri = pkg_resources.load_entry_point(
+        'newt.db', 'zodburi.resolvers', 'newt')
+    factory, dbkw = resolve_uri(uri)
+    with mock.patch("newt.db.storage") as storage:
+        factory()
+        (dsn,), options = storage.call_args
+        return dsn, options, dbkw
+
 class ZODBURITests(unittest.TestCase):
 
-    def parse(self, uri):
-        resolve_uri = pkg_resources.load_entry_point(
-            'newt.db', 'zodburi.resolvers', 'newt')
-        factory, dbkw = resolve_uri(uri)
-        with mock.patch("newt.db.storage") as storage:
-            factory()
-            (dsn,), options = storage.call_args
-            return dsn, options, dbkw
-
     def test_minimal(self):
-        self.assertEqual(('postgresql://', {}, {}), self.parse("newt://"))
+        self.assertEqual(('postgresql://', {}, {}), parse("newt://"))
 
     def test_full(self):
         self.assertEqual(
@@ -25,36 +25,36 @@ class ZODBURITests(unittest.TestCase):
                   database_name='main',
                   ),
              ),
-            self.parse("newt://jim:123@foo.bar/mydb"
-                       "?params1=value1"
-                       "&keep_history=True"
-                       "&driver=psycopg2"
-                       "&connection_pool_size=1"
-                       "&connection_cache_size=3"
-                       "&database_name=main"
-                       "&param2=value3"
-                       ),
+            parse("newt://jim:123@foo.bar/mydb"
+                  "?params1=value1"
+                  "&keep_history=True"
+                  "&driver=psycopg2"
+                  "&connection_pool_size=1"
+                  "&connection_cache_size=3"
+                  "&database_name=main"
+                  "&param2=value3"
+                  ),
             )
 
         def test_bool_variations(self):
             self.assertEqual(('postgresql://', dict(keep_history=True), {}),
-                              self.parse("newt://?keep_history=True"))
+                              parse("newt://?keep_history=True"))
             self.assertEqual(('postgresql://', dict(keep_history=True), {}),
-                              self.parse("newt://?keep_history=TRUE"))
+                              parse("newt://?keep_history=TRUE"))
             self.assertEqual(('postgresql://', dict(keep_history=True), {}),
-                              self.parse("newt://?keep_history=trUe"))
+                              parse("newt://?keep_history=trUe"))
             self.assertEqual(('postgresql://', dict(keep_history=True), {}),
-                              self.parse("newt://?keep_history=YeS"))
+                              parse("newt://?keep_history=YeS"))
             self.assertEqual(('postgresql://', dict(keep_history=True), {}),
-                              self.parse("newt://?keep_history=1"))
+                              parse("newt://?keep_history=1"))
 
             self.assertEqual(('postgresql://', dict(keep_history=False), {}),
-                              self.parse("newt://?keep_history=False"))
+                              parse("newt://?keep_history=False"))
             self.assertEqual(('postgresql://', dict(keep_history=False), {}),
-                              self.parse("newt://?keep_history=FALSE"))
+                              parse("newt://?keep_history=FALSE"))
             self.assertEqual(('postgresql://', dict(keep_history=False), {}),
-                              self.parse("newt://?keep_history=fAlse"))
+                              parse("newt://?keep_history=fAlse"))
             self.assertEqual(('postgresql://', dict(keep_history=False), {}),
-                              self.parse("newt://?keep_history=nO"))
+                              parse("newt://?keep_history=nO"))
             self.assertEqual(('postgresql://', dict(keep_history=False), {}),
-                              self.parse("newt://?keep_history=0"))
+                              parse("newt://?keep_history=0"))
